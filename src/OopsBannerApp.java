0a40e49f6d8e42789058cc2614e364d5bbd266c8
tk5323-web <tk5323@srmist.edu.in>
2026-03-03 18:17:56 +0530

    UC2

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -1,6 +1,12 @@
-public class OopsBannerApp {
+public class OopsBannerApp{
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println("   ***   "+" "+"   ***   "+" "+"******   "+" "+"    *****");
+        System.out.println(" **   ** "+" "+" **   ** "+" "+"**    ** "+" "+" ***     ");
+        System.out.println("**     **"+" "+"**     **"+" "+"**     **"+" "+"***      ");
+        System.out.println("**     **"+" "+"**     **"+" "+"**   **  "+" "+"   ***   ");
+        System.out.println("**     **"+" "+"**     **"+" "+"******   "+" "+"      ***");
+        System.out.println(" **   ** "+" "+" **   ** "+" "+"**       "+" "+"     *** ");
+        System.out.println("   ***   "+" "+"   ***   "+" "+"**       "+" "+"*****    ");
     }
 }
