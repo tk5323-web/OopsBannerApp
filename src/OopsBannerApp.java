52a7c2cb369789d1532d9bf63fa0d279c7759350
tk5323-web <tk5323@srmist.edu.in>
2026-03-03 18:23:48 +0530

    UC6

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -1,30 +1,42 @@
 public class OopsBannerApp{
 
     public static void main(String[] args) {
-
-        String[] bannerLines = createBanner();
-        printBanner(bannerLines);
-
-    }
-
-    public static String[] createBanner() {
-
-        return new String[] {
-
-            String.join("", "  *****          *****          *******         ******* "),
-            String.join("", "*       *      *       *       *        *      *         "),
-            String.join("", "*       *      *       *       *        *      *         "),
-            String.join("", "*       *      *       *       *******          *******"),
-            String.join("", "*       *      *       *       *                        *"),
-            String.join("", "*       *      *       *       *                        *"),
-            String.join("", "  *****          *****         *                *******")
+        String[] bannerLines = {
+                String.join("", getOLine1(), "  ", getOLine1(), "  ", getPLine1(), "  ", getSLine1()),
+                String.join("", getOLine2(), "  ", getOLine2(), "  ", getPLine2(), "  ", getSLine2()),
+                String.join("", getOLine3(), "  ", getOLine3(), "  ", getPLine3(), "  ", getSLine3()),
+                String.join("", getOLine4(), "  ", getOLine4(), "  ", getPLine4(), "  ", getSLine4()),
+                String.join("", getOLine5(), "  ", getOLine5(), "  ", getPLine5(), "  ", getSLine5()),
+                String.join("", getOLine6(), "  ", getOLine6(), "  ", getPLine6(), "  ", getSLine6()),
+                String.join("", getOLine7(), "  ", getOLine7(), "  ", getPLine7(), "  ", getSLine7())
         };
-    }
-
-    public static void printBanner(String[] bannerLines) {
 
         for (String line : bannerLines) {
             System.out.println(line);
         }
     }
+
+    public static String getOLine1() { return "  *****  "; }
+    public static String getOLine2() { return "*       *"; }
+    public static String getOLine3() { return "*       *"; }
+    public static String getOLine4() { return "*       *"; }
+    public static String getOLine5() { return "*       *"; }
+    public static String getOLine6() { return "*       *"; }
+    public static String getOLine7() { return "  *****  "; }
+
+    public static String getPLine1() { return "******* "; }
+    public static String getPLine2() { return "*      *"; }
+    public static String getPLine3() { return "*      *"; }
+    public static String getPLine4() { return "******* "; }
+    public static String getPLine5() { return "*      "; }
+    public static String getPLine6() { return "*      "; }
+    public static String getPLine7() { return "*      "; }
+
+    public static String getSLine1() { return "******* "; }
+    public static String getSLine2() { return "*      "; }
+    public static String getSLine3() { return "*      "; }
+    public static String getSLine4() { return "******* "; }
+    public static String getSLine5() { return "       *"; }
+    public static String getSLine6() { return "       *"; }
+    public static String getSLine7() { return " ******* "; }
 }
