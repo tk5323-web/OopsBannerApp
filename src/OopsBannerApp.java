9d3e92a6bd73cd549a71f4c9d58d6cdaea372c6b
tk5323-web <tk5323@srmist.edu.in>
2026-03-03 18:22:37 +0530

    UC5

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -2,7 +2,14 @@ public class OopsBannerApp{
 
     public static void main(String[] args) {
 
-        String[] bannerLines = {
+        String[] bannerLines = createBanner();
+        printBanner(bannerLines);
+
+    }
+
+    public static String[] createBanner() {
+
+        return new String[] {
 
             String.join("", "  *****          *****          *******         ******* "),
             String.join("", "*       *      *       *       *        *      *         "),
@@ -12,6 +19,9 @@ public class OopsBannerApp{
             String.join("", "*       *      *       *       *                        *"),
             String.join("", "  *****          *****         *                *******")
         };
+    }
+
+    public static void printBanner(String[] bannerLines) {
 
         for (String line : bannerLines) {
             System.out.println(line);
