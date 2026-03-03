2eda988429eb96a6ef2ecb236cad39b49dcecd5f
tk5323-web <tk5323@srmist.edu.in>
2026-03-03 18:20:38 +0530

    UC4

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -2,54 +2,19 @@ public class OopsBannerApp{
 
     public static void main(String[] args) {
 
-        String line1 = String.join("",
-                "  *****  ",   // O
-                "  *****  ",   // O
-                " ******  ",   // P
-                "  ****** ");  // S
+        String[] bannerLines = {
 
-        String line2 = String.join("",
-                " *     * ",   // O
-                " *     * ",   // O
-                " *     * ",   // P
-                " *       ");  // S
+            String.join("", "  *****          *****          *******         ******* "),
+            String.join("", "*       *      *       *       *        *      *         "),
+            String.join("", "*       *      *       *       *        *      *         "),
+            String.join("", "*       *      *       *       *******          *******"),
+            String.join("", "*       *      *       *       *                        *"),
+            String.join("", "*       *      *       *       *                        *"),
+            String.join("", "  *****          *****         *                *******")
+        };
 
-        String line3 = String.join("",
-                " *     * ",
-                " *     * ",
-                " *     * ",
-                " *       ");
-
-        String line4 = String.join("",
-                " *     * ",
-                " *     * ",
-                " ******  ",
-                "  *****  ");
-
-        String line5 = String.join("",
-                " *     * ",
-                " *     * ",
-                " *       ",
-                "       * ");
-
-        String line6 = String.join("",
-                " *     * ",
-                " *     * ",
-                " *       ",
-                "       * ");
-
-        String line7 = String.join("",
-                "  *****  ",
-                "  *****  ",
-                " *       ",
-                " ******  ");
-
-        System.out.println(line1);
-        System.out.println(line2);
-        System.out.println(line3);
-        System.out.println(line4);
-        System.out.println(line5);
-        System.out.println(line6);
-        System.out.println(line7);
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
     }
 }
